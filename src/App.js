408f07e97bd7492359b7b0b54a06561173049267
rzkzx <romanzidanramadhan@gmail.com>
2021-11-17 21:58:35 +0800

    React Navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {SplashScreen} from './pages';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
-  return <SplashScreen />;
+  return (
+    <NavigationContainer>
+      <SplashScreen />
+    </NavigationContainer>
+  );
 };
 
 export default App;
