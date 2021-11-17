a65f1c8d109b8b707865304171857bc391573bca
rzkzx <romanzidanramadhan@gmail.com>
2021-11-17 20:52:24 +0800

    Slicing Splash Screen

--- src/App.js
+++ src/App.js
@@ -7,24 +7,10 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {SplashScreen} from './pages';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <SafeAreaView style={backgroundStyle}>
-        <Text>Hello from Developer</Text>
-      </SafeAreaView>
-    </>
-  );
+  return <SplashScreen />;
 };
 
 export default App;
---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Logo} from '../../assets';
+
+export default function SplashScreen() {
+  return (
+    <View style={styles.page}>
+      <Logo />
+      <Text style={styles.title}>FoodMarket</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: '#F87F3B',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    marginTop: 5,
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
