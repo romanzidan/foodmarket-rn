0b540fbff7cd11baeaeaa41f143faae8549b1b3a
rzkzx <romanzidanramadhan@gmail.com>
2021-11-17 19:00:05 +0800

    Atomic Design

---
+++ src/App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <SafeAreaView style={backgroundStyle}>
+        <Text>Hello from Developer</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
