e24ea70faa63b0d92a94af5ea8a443b9c5d61366
rzkzx <romanzidanramadhan@gmail.com>
2021-11-19 08:04:04 +0800

    Add Ripple and OnPress

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 
-const Button = ({secondary, title}) => {
+const Button = ({secondary, title, onPress}) => {
   return (
     <View style={styles.container(secondary)}>
-      <Text style={styles.text}>{title}</Text>
+      <Pressable
+        android_ripple={{
+          color: secondary ? '#494949' : '#C84902',
+          borderless: false,
+          radius: 100,
+        }}
+        onPress={onPress}>
+        <Text style={styles.text}>{title}</Text>
+      </Pressable>
     </View>
   );
 };
@@ -14,13 +22,14 @@ export default Button;
 const styles = StyleSheet.create({
   container: secondary => ({
     backgroundColor: secondary ? '#8D92A3' : '#F87F3B',
-    padding: 15,
     borderRadius: 8,
+    overflow: 'hidden',
   }),
   text: {
     textAlign: 'center',
     color: '#ffffff',
     fontSize: 14,
     fontFamily: 'Poppins-Regular',
+    padding: 15,
   },
 });
