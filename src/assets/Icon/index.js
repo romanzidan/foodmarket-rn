cd11521d3d89d887db4286ddfe5e1312d0a60f55
rzkzx <romanzidanramadhan@gmail.com>
2021-11-20 11:07:29 +0800

    Add Icon Bottom Tab Navigator

--- src/assets/Icon/index.js
+++ src/assets/Icon/index.js
@@ -1,3 +1,17 @@
 import IcBack from './ic-back.svg';
+import IcHome from './ic-home.svg';
+import IcHomeOff from './ic-home-off.svg';
+import IcOrder from './ic-order.svg';
+import IcOrderOff from './ic-order-off.svg';
+import IcProfile from './ic-profile.svg';
+import IcProfileOff from './ic-profile-off.svg';
 
-export {IcBack};
+export {
+  IcBack,
+  IcHome,
+  IcHomeOff,
+  IcOrder,
+  IcOrderOff,
+  IcProfile,
+  IcProfileOff,
+};
