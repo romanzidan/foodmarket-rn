3ec58e8b029b90cc16dbe188f5bdd0756445f4ab
rzkzx <romanzidanramadhan@gmail.com>
2021-11-20 11:25:12 +0800

    Welcom User - Slicing Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,49 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {DummyProfile} from '../../assets';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.page}>
+      <View style={styles.profileContainer}>
+        <View>
+          <Text style={styles.appName}>FoodMarket</Text>
+          <Text style={styles.desc}>Let’s get some foods</Text>
+        </View>
+        <Image source={DummyProfile} style={styles.profile} />
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: '#FAFAFC',
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 24,
+    paddingTop: 30,
+    backgroundColor: '#ffffff',
+  },
+  appName: {
+    fontSize: 22,
+    fontFamily: 'Poppins-SemiBold',
+    color: '#020202',
+  },
+  desc: {
+    fontSize: 14,
+    marginTop: -5,
+    fontFamily: 'Poppins-Regular',
+    color: '#8D92A3',
+  },
+  profile: {
+    width: 50,
+    height: 50,
+    borderRadius: 8,
+  },
+});
