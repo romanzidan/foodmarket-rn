0df1be0e5667b13390e8d813b46cfec8e44aed5d
rzkzx <romanzidanramadhan@gmail.com>
2021-11-21 08:20:36 +0800

    Add Dummy Food Image

--- src/assets/Dummy/index.js
+++ src/assets/Dummy/index.js
@@ -1,6 +1,6 @@
 import DummyProfile from './ProfileDummy.png';
 import DummyFood1 from './DummyFood1.png';
-import DummyFood2 from './DummySmallFood2.png';
+import DummyFood2 from './DummyFood2.png';
 import DummySmallFood1 from './DummySmallFood1.png';
 import DummySmallFood2 from './DummySmallFood2.png';
 import DummySmallFood3 from './DummySmallFood3.png';
